d4a58acb8697bbba38355c700b4f3191b0b240a7
Shiva Kumar <shivam518518@gmail.com>
2025-09-22 19:01:33 +0530

    done

---
+++ lib/core_process_guide/View/core_process_guide_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CoreProcessGuideList extends StatefulWidget {
+  const CoreProcessGuideList({super.key});
+
+  @override
+  State<CoreProcessGuideList> createState() => _CoreProcessGuideListState();
+}
+
+class _CoreProcessGuideListState extends State<CoreProcessGuideList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.black);
+  }
+}
